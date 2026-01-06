96bc885a9b7619fdb6f0aa5f1bbbcc36fe8b7acf
SuthidaDamchum <suthida.damchum@hotmail.com>
2026-01-06 22:30:46 +0100

    Implemented UpdateBook function to execute in EditNewBookTitleDialog.

--- BookStore_Presentation/Services/BookSelectionService.cs
+++ BookStore_Presentation/Services/BookSelectionService.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using BookStore_Domain;
+using BookStore_Infrastrcuture.Data.Model;
 using BookStore_Presentation.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace BookStore_Presentation.Services
 {
@@ -11,8 +9,32 @@ namespace BookStore_Presentation.Services
     {
         public BookAdminItem? SelectedBook { get; set; }
 
-        public void UpdateBook()
+
+        private readonly BookStoreContext _context;
+
+        public BookSelectionService(BookStoreContext context)
         {
+            _context = context;
+        }
+        public Book UpdateBook(
+            string isbn13, string title, string? language, decimal? price, DateOnly? publicationDate, int? pagecount, int? genreID, int? publisherId)
+        {
+            var book = _context.Books.FirstOrDefault(b => b.Isbn13 == isbn13);
+
+            if (book == null)
+                throw new Exception("Book not found");
+
+
+            book.Title = title;
+            book.Language = language;
+            book.Price = price;
+            book.PublicationDate = publicationDate;
+            book.PageCount = pagecount;
+            book.GenreId = genreID;
+            book.PublisherId = publisherId;
+
+            _context.SaveChanges();
+            return book;
 
         }
     }
