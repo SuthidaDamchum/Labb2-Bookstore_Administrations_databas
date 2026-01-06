3222237147489b798503a96d6f3853c62a8d657a
SuthidaDamchum <suthida.damchum@hotmail.com>
2026-01-06 11:51:32 +0100

    Added author sevice

--- BookStore_Presentation/App.xaml.cs
+++ BookStore_Presentation/App.xaml.cs
@@ -1,36 +1,28 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Runtime.Serialization.DataContracts;
-using System.Windows;
+﻿using System.Windows;
+using BookStore_Infrastrcuture.Data.Model;
 using BookStore_Presentation.Services;
 using BookStore_Presentation.ViewModels;
 
 namespace BookStore_Presentation
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
-          protected override void OnStartup(StartupEventArgs e)
+        protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
-            // ✅ Create the shared service
+
             var bookSelectionService = new BookSelectionService();
-
-            // ✅ Create the ViewModels with the service
-
-            var booksVm = new BooksAdminViewModel(bookSelectionService);
-            var inventoryVm = new InventoryByStoreViewModel(bookSelectionService, booksVm); // pass shared instance
-            // ✅ Create the main window and assign DataContexts
-
-            var mainWindow = new MainWindow
-                {
-                   DataContext = inventoryVm  // or booksVm depending on your window
-                };
+            var authorService = new AuthorService(new BookStoreContext());
 
          
+            var mainWindowViewModel = new MainWindowViewModel(bookSelectionService, authorService);
 
+            var mainWindow = new MainWindow
+            {
+                DataContext = mainWindowViewModel
+            };
+
+            mainWindow.Show();
         }
     }
 }
