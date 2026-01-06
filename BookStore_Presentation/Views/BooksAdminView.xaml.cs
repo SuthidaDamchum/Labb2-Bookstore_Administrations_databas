eaa03ffe910b8f5ed45487b106b4d76cb7729373
SuthidaDamchum <suthida.damchum@hotmail.com>
2026-01-06 11:47:07 +0100

    Updated design for books view

--- BookStore_Presentation/Views/BooksAdminView.xaml.cs
+++ BookStore_Presentation/Views/BooksAdminView.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows.Controls;
+using BookStore_Infrastrcuture.Data.Model;
+using BookStore_Presentation.Services;
+using BookStore_Presentation.ViewModels;
 
 namespace BookStore_Presentation.Views
 {
@@ -24,7 +14,5 @@ namespace BookStore_Presentation.Views
         {
             InitializeComponent();
         }
-
-     
     }
 }
