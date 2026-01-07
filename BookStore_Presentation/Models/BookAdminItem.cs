8e680a8c010c21b17eea80db26a217f3f43716b8
SuthidaDamchum <suthida.damchum@hotmail.com>
2026-01-07 16:26:11 +0100

    Added authorId property

--- BookStore_Presentation/Models/BookAdminItem.cs
+++ BookStore_Presentation/Models/BookAdminItem.cs
@@ -10,14 +10,14 @@ namespace BookStore_Presentation.Models
     {
         public string Isbn13 { get; set; } = "";
         public string Title { get; set; } = "";
-        public string AuthorNames { get; set; } = "";
+        public string AuthorNameString { get; set; } = "";
         public string GenreName { get; set; } = "";
         public string PublisherName { get; set; } = "";
         public string? Language { get; set; }
         public decimal Price { get; set; }
         public DateOnly? PublicationDate { get; set; }
         public int? PageCount { get; set; }
-
+        public int[] AuthorIds { get; set; } = [];
     }
 }
 
