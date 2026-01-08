9fbab3f5df747a7a2f4875d71033aba270982f5b
SuthidaDamchum <suthida.damchum@hotmail.com>
2026-01-08 21:45:49 +0100

    created async delegate to be able to call async commands

---
+++ BookStore_Presentation/Command/AsyncDelegateCommand.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace BookStore_Presentation.Command;
+
+public class AsyncDelegateCommand : ICommand
+{
+    private readonly Func<object?, Task> _executeAsync;
+    private readonly Func<object?, bool>? _canExecute;
+    private bool _isExecuting;
+
+    public AsyncDelegateCommand(Func<object?, Task> executeAsync, Func<object?, bool>? canExecute = null)
+    {
+        ArgumentNullException.ThrowIfNull(executeAsync);
+        _executeAsync = executeAsync;
+        _canExecute = canExecute;
+    }
+
+    public bool CanExecute(object? parameter) =>
+        !_isExecuting && (_canExecute?.Invoke(parameter) ?? true);
+
+    public async void Execute(object? parameter)
+    {
+        if (!CanExecute(parameter)) return;
+        try
+        {
+            _isExecuting = true;
+            RaiseCanExecuteChanged();
+            await _executeAsync(parameter);
+        }
+        finally
+        {
+            _isExecuting = false;
+            RaiseCanExecuteChanged();
+        }
+    }
+
+    public event EventHandler? CanExecuteChanged;
+    public void RaiseCanExecuteChanged() =>
+        CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+}
