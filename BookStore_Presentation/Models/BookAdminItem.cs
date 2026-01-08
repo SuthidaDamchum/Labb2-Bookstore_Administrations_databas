67388488a4618dfd34e1127544839ca54dd9c5ad
SuthidaDamchum <suthida.damchum@hotmail.com>
2026-01-08 15:55:02 +0100

    Implemedted ViewModelBase to BookAdminItem

--- BookStore_Presentation/Models/BookAdminItem.cs
+++ BookStore_Presentation/Models/BookAdminItem.cs
@@ -3,20 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BookStore_Presentation.ViewModels;
 
 namespace BookStore_Presentation.Models
 {
-    public class BookAdminItem
+    public class BookAdminItem : ViewModelBase
     {
         public string Isbn13 { get; set; } = "";
         public string Title { get; set; } = "";
-        public string AuthorNameString { get; set; } = "";
-        public string GenreName { get; set; } = "";
-        public string PublisherName { get; set; } = "";
         public string? Language { get; set; }
         public decimal Price { get; set; }
-        public DateOnly? PublicationDate { get; set; }
         public int? PageCount { get; set; }
+        public string GenreName { get; set; } = "";
+        public DateOnly? PublicationDate { get; set; }
+        public string AuthorNameString { get; set; } = "";
+        public string PublisherName { get; set; } = "";
         public int[] AuthorIds { get; set; } = [];
     }
 }
